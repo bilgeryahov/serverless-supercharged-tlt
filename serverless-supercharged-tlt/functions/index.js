79184aab0009dc9622b5cd352f8ea4d7bc246d90
bilgeryahov <bayahov1@gmail.com>
2019-07-18 14:32:59 +0300

    Implement logic for the transaction logging procedure.

--- serverless-supercharged-tlt/functions/index.js
+++ serverless-supercharged-tlt/functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = {};
+
+const WATCH_REF = '/messages/{messageId}';
+const LOG_REF = '/messages_logs';
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://serverless-supercharged-tlt.firebaseio.com'
+});
+
+exports.transactionLog = functions.database.ref(WATCH_REF).onWrite((change, context) =>
+    admin.database().ref(LOG_REF).push().set({
+        type: change.before.exists() ? (change.after.exists() ? 'update' : 'delete') : 'create',
+        resource: context.resource,
+        author: context.auth.uid,
+        occuredAt: context.timestamp,
+        id: context.eventId
+    }));
